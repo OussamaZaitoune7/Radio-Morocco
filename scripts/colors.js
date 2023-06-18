4f6dfab10a35a211964cedf5f7982d79985d1175
OussamaZaitoune7 <developer.zaitoune@gmail.com>
2023-06-18 10:11:09 +0100

    lastVR

--- scripts/colors.js
+++ scripts/colors.js
@@ -1,10 +1,12 @@
 class Colors{
     constructor(){
         if(localStorage.getItem("SaveColor")==null){
-    
-            document.body.style.background = "linear-gradient(to right, #00d2ff, #928dab)";
-       document.getElementById("footer").style.background = "linear-gradient(to right, #00d2ff, #928dab)";
-       
+            this.header.style.background ="linear-gradient(to right,#00d2ff, #928dab)";
+            this.header.style.borderLeft ="0.5px solid #928dab";
+            this.header.style.borderTop ="0.5px solid #928dab";
+            document.getElementById("footer").style.borderLeft ="0.5px solid #928dab";
+            document.body.style.background = "#00d2ff";
+            document.getElementById("footer").style.background = "linear-gradient(to right, #43978D, #F9E07F)";
         }
 
     this.color1 = document.getElementById("color-1");
@@ -56,44 +58,67 @@ this.color6 = document.getElementById("color-6");
         this.header = document.getElementById("header");
         if(color == "color-1"){
             this.header.style.background ="linear-gradient(to right, #43978D, #F9E07F)";
-            document.body.style.background = "linear-gradient(to right, #43978D, #F9E07F)";
+            this.header.style.borderLeft ="0.5px solid #F9E07F";
+            this.header.style.borderTop ="0.5px solid #F9E07F";
+            document.getElementById("footer").style.borderLeft ="0.5px solid #F9E07F";
+            document.body.style.background = "#43978d";
             document.getElementById("footer").style.background = "linear-gradient(to right, #43978D, #F9E07F)";
         }
         else if(color == "color-2"){
             this.header.style.background ="linear-gradient(to right, #CCABD8, #E4C787)";
-            document.body.style.background ="linear-gradient(to right, #CCABD8, #E4C787)";
+            this.header.style.borderLeft ="0.5px solid #E4C787";
+            this.header.style.borderTop ="0.5px solid #E4C787";
+            document.getElementById("footer").style.borderLeft ="0.5px solid #E4C787";
+            document.body.style.background ="#CCABD8";
             document.getElementById("footer").style.background = "linear-gradient(to right, #CCABD8, #E4C787)";
         }
 
         else if(color == "color-3"){
             this.header.style.background ="linear-gradient(to right, #A7D676, #85CBCC)";
-            document.body.style.background ="linear-gradient(to right, #A7D676, #85CBCC)";
+            this.header.style.borderLeft ="0.5px solid #85CBCC";
+            this.header.style.borderTop ="0.5px solid #85CBCC";
+            document.body.style.background ="#A7D676";
             document.getElementById("footer").style.background = "linear-gradient(to right, #A7D676, #85CBCC)";
         }
 
         else if(color == "color-4"){
             this.header.style.background = "linear-gradient(to right, #3C4CAD, #F9C449)";
-            document.body.style.background = "linear-gradient(to right, #3C4CAD, #F9C449)";
+            this.header.style.borderLeft ="0.5px solid #F9C449";
+            this.header.style.borderTop ="0.5px solid #F9C449";
+            document.getElementById("footer").style.borderLeft ="0.5px solid #F9C449";
+            document.body.style.background = "#3C4CAD";
             document.getElementById("footer").style.background = "linear-gradient(to right, #3C4CAD, #F9C449)";
         }
         else if(color == "color-5"){
-        this.header.style.background = "linear-gradient(to right, #3C4CAD, #F9C449)";
-        document.body.style.background = "linear-gradient(to right, #3C4CAD, #F9C449)";
-        document.getElementById("footer").style.background = "linear-gradient(to right, #3C4CAD, #F9C449)";
+        this.header.style.background = "linear-gradient(to right, #5a565a, #4d3d)";
+            this.header.style.borderLeft ="0.5px solid #5a565a";
+            this.header.style.borderTop ="0.5px solid #5a565a";
+            document.getElementById("footer").style.borderLeft ="0.5px solid #5a565a";
+        document.body.style.background = "#4d463d";
+        document.getElementById("footer").style.background = "linear-gradient(to right, #5a565a, #4d3d)";
         }
         else if(color == "color-6"){
-        this.header.style.background = "linear-gradient(to right, #3C4CAD, #F9C449)";
-        document.body.style.background = "linear-gradient(to right, #3C4CAD, #F9C449)";
-        document.getElementById("footer").style.background = "linear-gradient(to right, #3C4CAD, #F9C449)";
+        this.header.style.background = "linear-gradient(to right, #623596, #069bc9)";
+            this.header.style.borderLeft ="0.5px solid #069bc9";
+            this.header.style.borderTop ="0.5px solid #069bc9";
+            document.getElementById("footer").style.borderLeft ="0.5px solid #069bc9";
+        document.body.style.background = "#623596";
+        document.getElementById("footer").style.background = "linear-gradient(to right, #623596, #069bc9)";
         }
         else if(color == "color-7"){
         this.header.style.background = "linear-gradient(to right, yellowgreen, orangered)";
-        document.body.style.background = "linear-gradient(to right, yellowgreen, orangered)";
+            this.header.style.borderLeft ="0.5px solid orangered";
+            this.header.style.borderTop ="0.5px solid orangered";
+            document.getElementById("footer").style.borderLeft ="0.5px solid orangered";
+        document.body.style.background = "yellowgreen";
         document.getElementById("footer").style.background = "linear-gradient(to right, yellowgreen, orangered)";
         }
         else if(color == "color-8"){
         this.header.style.background = "linear-gradient(to right, blue, red)";
-        document.body.style.background = "linear-gradient(to right, blue, red)";
+            this.header.style.borderLeft ="0.5px solid red";
+            this.header.style.borderTop ="0.5px solid red";
+            document.getElementById("footer").style.borderLeft ="0.5px solid red";
+        document.body.style.backgroundColor = "blue";
         document.getElementById("footer").style.background = "linear-gradient(to right, blue, red)";
         }
         localStorage.setItem("SaveColor",color);
@@ -101,3 +126,4 @@ this.color6 = document.getElementById("color-6");
 
 }
 onload = new Colors();
+// document.body.style.backgroundImage = "linear-gradient(to right, #00d2ff, #928dab";
